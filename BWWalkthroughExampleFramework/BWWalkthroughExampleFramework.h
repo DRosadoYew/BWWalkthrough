11b0475fb37c1d87c8d14615f2df78b670c8d895
Daniel Rosado <dsrosado@gmail.com>
2015-12-09 01:41:30 +0100

    Add framework target with shared scheme.

---
+++ BWWalkthroughExampleFramework/BWWalkthroughExampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  BWWalkthroughExampleFramework.h
+//  BWWalkthroughExampleFramework
+//
+//  Created by Daniel Rosado on 09/12/15.
+//  Copyright © 2015 Yari D'areglia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BWWalkthroughExampleFramework.
+FOUNDATION_EXPORT double BWWalkthroughExampleFrameworkVersionNumber;
+
+//! Project version string for BWWalkthroughExampleFramework.
+FOUNDATION_EXPORT const unsigned char BWWalkthroughExampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BWWalkthroughExampleFramework/PublicHeader.h>
+
+
